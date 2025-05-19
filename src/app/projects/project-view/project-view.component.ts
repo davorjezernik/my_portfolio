43a93c66b100287975cf78153333964a0e121aae
Davor Jezernik <369davor@gmail.com>
2025-05-19 21:26:29 +0200

    template data uppdate not working

--- src/app/projects/project-view/project-view.component.ts
+++ src/app/projects/project-view/project-view.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { NavbarComponent } from '../../shared/navbar/navbar.component';
 import { TemplateItem, templateLayout } from '../../../models/template';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-project-view',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, NgIf],
   templateUrl: './project-view.component.html',
   styleUrl: './project-view.component.scss'
 })
@@ -13,10 +14,26 @@ export class ProjectViewComponent implements OnChanges {
   @Input() templateID: string = '';
 
   project: TemplateItem[] = templateLayout;
+  selectedTemplate: TemplateItem | undefined;
+  templateMap: Map<string, TemplateItem> = new Map();
+
+  constructor() {
+    this.project.forEach(item => this.templateMap.set(item.id, item));
+  }
 
   ngOnChanges(changes: SimpleChanges): void {
-    if (changes['templateID']) {
-      console.log('Template to use:', this.templateID);
+    if (
+      changes['templateID'] &&
+      changes['templateID'].currentValue !== changes['templateID'].previousValue &&
+      this.templateID
+    ) {
+      let foundTemplate = this.templateMap.get(this.templateID);
+      if (foundTemplate) {
+        this.selectedTemplate = foundTemplate;
+        console.log('Template data', this.selectedTemplate)
+      }
     }
   }
+
+
 }
