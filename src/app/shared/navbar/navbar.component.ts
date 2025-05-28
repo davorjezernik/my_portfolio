6abc1388a61fbfb737da1f5be88706b5f1efa297
Davor Jezernik <369davor@gmail.com>
2025-05-28 18:14:11 +0200

    langueg select

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  sliderValue = 1; 
 
+  setLanguage(value: number): void {
+    this.sliderValue = value;
+  }
 }
