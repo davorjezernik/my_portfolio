c3cb308252863d92a3b6fcd5c228fe912d7d61f8
Davor Jezernik <369davor@gmail.com>
2025-07-29 21:10:59 +0200

    Responsive uppdate project view component add forms uppdate

--- src/app/ready-to-work/ready-to-work.component.ts
+++ src/app/ready-to-work/ready-to-work.component.ts
@@ -3,10 +3,11 @@ import { FooterComponent } from '../shared/footer/footer.component';
 import { TranslateModule } from '@ngx-translate/core';
 import { FormsModule } from '@angular/forms';
 import { NgForm } from '@angular/forms';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-ready-to-work',
-  imports: [FooterComponent, TranslateModule, FormsModule],
+  imports: [FooterComponent, TranslateModule, FormsModule, NgIf],
   templateUrl: './ready-to-work.component.html',
   styleUrl: './ready-to-work.component.scss'
 })
@@ -19,13 +20,17 @@ export class ReadyToWorkComponent {
     acceptTerms: false
   }
 
+  formSubmitted = false;
+
   submitForm(form: NgForm) {
-  if (form.valid) {
-    this.onSubmit();
-  } else {
-    console.warn('Form is invalid');
+    this.formSubmitted = true;
+
+    if (form.valid) {
+      this.onSubmit();
+    } else {
+      console.warn('Form is invalid');
+    }
   }
-}
 
   onSubmit() {
     console.log('Form submitted:', this.contactData);
