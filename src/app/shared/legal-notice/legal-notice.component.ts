5960b86e4d732772c67fe6ee3f72fc1ba821d6c3
Davor Jezernik <369davor@gmail.com>
2025-08-02 19:08:06 +0200

    footer responsive workup to match figma, legal notice finished and designed

--- src/app/shared/legal-notice/legal-notice.component.ts
+++ src/app/shared/legal-notice/legal-notice.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { TranslateModule } from '@ngx-translate/core';
+import { FooterComponent } from '../footer/footer.component'
 
 @Component({
   selector: 'app-legal-notice',
-  imports: [NavbarComponent, TranslateModule],
+  imports: [NavbarComponent, TranslateModule, FooterComponent],
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss'
 })
