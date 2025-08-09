281a71897a14434ae8c3d25407432cf1d7793dbd
Davor Jezernik <369davor@gmail.com>
2025-08-09 21:45:38 +0200

    privacy policy added with responsives, and small scss tweeks

--- src/app/shared/privacy-policy/privacy-policy.component.ts
+++ src/app/shared/privacy-policy/privacy-policy.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
 import { TranslateModule } from '@ngx-translate/core';
+import { FooterComponent } from '../footer/footer.component'
 
 @Component({
   selector: 'app-privacy-policy',
-  imports: [TranslateModule],
+  imports: [NavbarComponent, TranslateModule, FooterComponent],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss'
 })
