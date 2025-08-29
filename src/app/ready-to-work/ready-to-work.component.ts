e691f6a1c6766f1e0e7557fe6255607764510527
Davor Jezernik <369davor@gmail.com>
2025-08-29 19:00:35 +0200

    project-view backround setup done, thoughst compontent contet fixed till 1200px

--- src/app/ready-to-work/ready-to-work.component.ts
+++ src/app/ready-to-work/ready-to-work.component.ts
@@ -52,13 +52,9 @@ export class ReadyToWorkComponent {
     if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
       this.http.post(this.post.endPoint, this.post.body(this.contactData))
         .subscribe({
-          next: (response) => {
+          next: () => {
             this.resetFormData(ngForm);
-          },
-          error: (error) => {
-            console.error(error);
-          },
-          complete: () => console.info('send post complete'),
+          }
         });
     } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
       this.resetFormData(ngForm);
